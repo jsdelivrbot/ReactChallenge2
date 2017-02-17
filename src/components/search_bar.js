66b66cc4cc4002f4696d244b1b715adf63211b79
Win <win.m179@gmail.com>
2017-02-17 14:13:42 +0000

    Refactored SearchBar functional component into Class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,18 @@
 // React is still needed even though we are not directly using React key word
 // This is because <input /> jsx syntax will be compiled into React.createElement
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+// // Functional based component hasn't got any ability to communicate with other components 
+// const SearchBar = () => {
+// 	return <input />;
+// };
+
+// Class based component is good for a component with lots of functionalities i.e events like when user input, hover, etc.
+// Every class based component in React must have render() method defined!
+class SearchBar extends Component {
+	render() {
+		return <input />;
+	}
+}
 
 export default SearchBar;
