62fa65ece4ae7cea414a1bc785e8ccb16252a9c6
Win <win.m179@gmail.com>
2017-02-24 16:31:39 +0000

    added debounce function to index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 // Otherwise React is going to return undefined
 // React module is used to create and manage components ONLY!
 // ReactDOM is used to put the created components to the DOM!
-import _ from 'loadash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 // Declare variable to store youtube API key
 const API_KEY = 'AIzaSyDcYnQtlPaUcMTEjrS-iosfKep06sGqX7s';
@@ -35,11 +35,13 @@ class App extends Component {
 		});
 	}
 
-	render(){
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
-				<VideoDetail video={this.state.seletedVideo}/>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.seletedVideo} />
 				<VideoList 
 				onVideoSelect={seletedVideo => this.setState({seletedVideo})} 
 				videos={this.state.videos} />
